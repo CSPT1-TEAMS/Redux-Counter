6e8a7e8b65607235d840e7095954c5b20380001e
unknown <alexisreyes90@gmail.com>
2018-04-17 22:31:22 -0400

    async stretch complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,6 +15,12 @@ class Counter extends Component {
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+
+        //timeout?
+
+        setTimeout(() => {
+            this.props.increment()
+        }, 1000)
     };
 
     render() {
@@ -35,9 +41,9 @@ class Counter extends Component {
                 <button onClick={this.incrementIfOdd}>
                     Increment if odd, decrement if even
                 </button>
-                {/* <button onClick={this.incrementAsync}>
+                <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
